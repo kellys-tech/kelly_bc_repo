b49d39ca9789c13fb71ca0682cc34630028e4dcc
Kelly Smith <kdalton77@hotmail.com>
2020-10-17 15:56:44 -0400

    create tx to set text adn store to ls

--- homework/05-third-party-apis/develop/script.js
+++ homework/05-third-party-apis/develop/script.js
@@ -3,53 +3,60 @@
 var nowDate = moment().format('dddd,' + ' MMM Do YYYY');
 var date = $('#currentDay');
 date.text(nowDate);
-console.log("what is the date?");
+// console.log("what is the date?");
 
-// create varible for time slots
-var time = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
-console.log(time)
+// create variable for time slots
+var time = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+// console.log(time)
 
 var i = 0;
 //function for checking and updating schedule with css styling
 function hourChecker(){
     var nowHour = moment().hours();
-    console.log("this moment");
+    console.log(nowHour);
 
     $(time).each(function(calHour) {
     var calHour = parseInt(time[i++]);
     console.log(calHour);
 
     if (calHour < nowHour) {
-        $(".time-block").addClass("past");
+        $("#time" + calHour).addClass("past");
     }
     else if (calHour === nowHour) {
-        $(".time-block").removeClass("past");
-        $(".time-block").addClass("present");
+        $("#time" + calHour).removeClass("past");
+        $("#time" + calHour).addClass("present");
     }
     else {
-        $(".time-block").removeClass("past");
-        $(".time-block").removeClass("present");
-        $(".time-block").addClass("future");
+        $("#time" + calHour).removeClass("past");
+        $("#time" + calHour).removeClass("present");
+        $("#time" + calHour).addClass("future");
     } 
     })}
 hourChecker();
 
-// function saveEvent()
-//when user enters text in text area
-// then clicks Save button
-// save text in text area
+//save calender event into local storage
+$(document).ready(function() {
 
+    $(".btn").on("click", function(){
+        var event = $(this).siblings("textarea").val()
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, event)
+        console.log(time, event)
+});
+});
 
-// get data from local storage
-// $("#hour9.description").val(localStorage.getItem("hour9"));
-// $("#hour10.description").val(localStorage.getItem("hour10"));
-// $("#hour11.description").val(localStorage.getItem("hour11"));
-// $("#hour12.description").val(localStorage.getItem("hour12"));
-// $("#hour1.description").val(localStorage.getItem("hour1"));
-// $("#hour2.description").val(localStorage.getItem("hour2"));
-// $("#hour3.description").val(localStorage.getItem("hour3"));
-// $("#hour4.description").val(localStorage.getItem("hour4"));
-// $("#hour5.description").val(localStorage.getItem("hour5"));
+var interval = setInterval(hourChecker, 900000)
+
+//get event from local storage
+$("#hour9.description").val(localStorage.getItem("hour9"));
+$("#hour10.description").val(localStorage.getItem("hour10"));
+$("#hour11.description").val(localStorage.getItem("hour11"));
+$("#hour12.description").val(localStorage.getItem("hour12"));
+$("#hour13.description").val(localStorage.getItem("hour13"));
+$("#hour14.description").val(localStorage.getItem("hour14"));
+$("#hour15.description").val(localStorage.getItem("hour15"));
+$("#hour16.description").val(localStorage.getItem("hour16"));
+$("#hour17.description").val(localStorage.getItem("hour17"));
 
 
 // GIVEN I am using a daily planner to create a schedule
@@ -58,10 +65,11 @@ hourChecker();
 // WHEN I scroll down
 // THEN I am presented with time blocks for standard business hours(done)
 // WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future - in progress
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future(done)
 // WHEN I click into a time block
-// THEN I can enter an event
+// THEN I can enter an event(done)
 // WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
+// THEN the text for that event is saved in local storage(done)
 // WHEN I refresh the page
 // THEN the saved events persist
+
